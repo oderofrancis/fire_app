d3eefc8964af2ea1957d014779a6567d84b0591f
oderofrancis <francisodero10@gmail.com>
2022-01-06 19:32:53 +0300

    update progress

--- report/forms.py
+++ report/forms.py
@@ -4,19 +4,20 @@ from django.forms import ModelForm
 from .models import *
 
 
-class IncidenceForm(geoforms.ModelForm):
+class IncidenceForm(forms.ModelForm):
 
     class Meta:
         model = Incidence
         fields = ('place','injuries','location')
+        
         widgets = {'location':  geoforms.OSMWidget(
         	attrs={
         	'map_width': 330, 
-        	'map_height': 500,
+        	'map_height': 400,
         	'default_lat':  -0.42768,
             'default_lon': 36.9580,
-    		'default_zoom':6,
+    		'default_zoom':9.5,
     		'max_zoom':20,
-    		'min_zoom':3,
+    		'min_zoom':5,
     		
         	})}
--- report/views.py
+++ report/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import *
 from .models import *
 from django.contrib import messages
 from django.core.serializers import serialize
-import pandas as pd
+import geopandas as gpd
 
 # Create your views here.
 
@@ -14,28 +14,26 @@ def report(request):
 
 	form = IncidenceForm()
 
-	if request.method =='POST':
-		form =  IncidenceForm(request.POST)
-		if form.is_valid():
-			form.save()
+	if request.method == 'POST':
+  		form = IncidenceForm(request.POST)
+  		if form.is_valid():
+  			form.save()
 
-			messages.success(request, 'we are working on the incidence reported,THANK YOU.')
-			return redirect('/')
+  			messages.success(request, 'we are working on the incidence reported,THANK YOU.')
+  			return redirect('/')
 
-		else:
-
-			messages.warning(request, form.errors)
-			return redirect('/report/')
+  		else:
+  			messages.warning(request, form.errors)
+  			return redirect('/report/')
 
 
 	# for the values
 
 	cases = Incidence.objects.all().count()
 	values = Incidence.objects.all()
-	values = serialize('json',values)
-	values = pd.read_json(values)
+	values = serialize('geojson',values)
+	values = gpd.read_file(values)
 	values = values[values.columns[1]].sum()
-	# values =2
 
 
 	context={'form':form,'cases':cases,'values':values}
